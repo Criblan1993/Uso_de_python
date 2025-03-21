970a5961bd9cec93abde7f2b86c9d0fa4400bbe8
Criblan1993 <criblan123@gmail.com>
2025-03-20 19:59:55 -0500

    Clase 20 de Marzo

---
+++ tipo_de_operadores.py
@@ -0,0 +1,85 @@
+'''
+operadores aritméticos
+20 de marzo
+
+'''
+
+# # #operador suma
+# # print(2+6)
+
+# # #operador resta
+# # print(8-4)
+
+# # #operador multiplicacion
+x=4
+y=6
+# # print(x*y) 
+
+# #operador división
+# z=12/4
+# print(z)
+# print(type(z)) #el resultado de la división siempre es flotante
+
+
+
+# #operador división piso(floor)
+# print(10/3) #división tradicional
+# print(10//3) #división piso
+# '''
+# la división piso siempre devuelve el entero más próximo hacia abajo
+# '''
+
+# #operador módulo
+# print(20/3)
+# print(20%3)
+
+
+# #operador división
+# print(8//-3)
+# print(8/-3)
+
+
+#operador potencia
+# print(x**2)
+
+
+
+#es igual a
+# print("enca"=="Enca")
+# print(8==8)
+# print(8=="8")
+# print("camilo"=="Camilo")
+# print(3==3.01)
+
+
+
+#es diferente de 
+# print("palabra"!="Palabra")
+
+#Asignación de igualdad o definición
+w=5
+
+#Incremento
+# saldo=100
+# saldo= saldo + 1
+# print(saldo)
+
+#Decrecimiento
+saldo_b=200
+#saldo= saldo + 1 
+saldo_b-=8
+print(saldo_b)
+
+'''
+operadore lógicos
+and: comprueba si todas las condiciones se cumplen, True , False de lo contrario
+or: comprueba si alguna de las condiciones se cumple, True , False de lo contrario
+not: negar el estado de una variable
+'''
+print(x==4 and y==6)
+print(x==5 or y==8)
+
+usuario_logueado=True
+click_logout=True
+print(not usuario_logueado)
+
